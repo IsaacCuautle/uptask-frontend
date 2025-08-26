71afa5b272a34044ad9a6403fbe5c0b5e6688d93
isaac <cuautle.tron@gmail.com>
2025-08-26 13:58:29 -0600

    creando el layout de la aplicacion

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import "./index.css";
+import Router from "./router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1 className='text-6xl'>UpTask</h1>
-  </StrictMode>,
-)
+    <Router />
+  </StrictMode>
+);
