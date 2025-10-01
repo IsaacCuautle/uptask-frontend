69bf0709b00a8c96a000fffec7475f9298636196
isaac <cuautle.tron@gmail.com>
2025-10-01 09:24:10 -0600

    creando la funcion para crear tareas

---
+++ src/API/tasksAPI.ts
@@ -0,0 +1,27 @@
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+
+import { type TaskFormData, type Project } from "../types";
+
+type TaskAPI = {
+  formData: TaskFormData;
+  projectID: Project["_id"];
+};
+
+export async function createTask({
+  formData,
+  projectID,
+}: Pick<TaskAPI, "formData" | "projectID">) {
+  try {
+    const url = `localhost:4000/api/projects/${projectID}/task`;
+    const { data } = await api.post<string>(url, formData);
+
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+
+  return;
+}
